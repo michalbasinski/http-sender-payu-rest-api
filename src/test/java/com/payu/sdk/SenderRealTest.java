897c2df91d62dfd8ea7acf44e85bfb6d07743100
Michał Basiński <michal.basinski88@gmail.com>
2015-05-23 20:25:22 +0200

    Moving converter to openpayu-messages

--- src/test/java/com/payu/sdk/SenderRealTest.java
+++ src/test/java/com/payu/sdk/SenderRealTest.java
@@ -11,12 +11,16 @@ import java.util.logging.Logger;
 import com.payu.sdk.commons.*;
 import com.payu.sdk.exception.WrongPayloadException;
 import com.payu.sdk.exception.WrongProtocolException;
-import com.payu.sdk.requests.OrderCreateRequest;
-import com.payu.sdk.requests.builders.OrderCreateRequestBuilder;
-import com.payu.sdk.requests.entities.Buyer;
-import com.payu.sdk.requests.entities.Product;
-import com.payu.sdk.requests.entities.builders.BuyerBuilder;
-import com.payu.sdk.requests.entities.builders.ProductBuilder;
+import com.payu.sdk.messages.converters.JSONConverter;
+import com.payu.sdk.messages.converters.ResponseType;
+import com.payu.sdk.messages.entities.Buyer;
+import com.payu.sdk.messages.entities.Product;
+import com.payu.sdk.messages.entities.builders.BuyerBuilder;
+import com.payu.sdk.messages.entities.builders.ProductBuilder;
+import com.payu.sdk.messages.request.OpenPayURequest;
+import com.payu.sdk.messages.request.OrderCreateRequest;
+import com.payu.sdk.messages.request.builders.OrderCreateRequestBuilder;
+import com.payu.sdk.messages.response.OrderCreateResponse;
 import org.apache.http.HttpResponse;
 import org.junit.Before;
 import org.junit.Test;
@@ -45,6 +49,20 @@ public class SenderRealTest extends AbstractTest {
         headers.put("Authorization", BasicAuthUtils.generateAuthorizationHeader(login, password));
         headers.put("Content-type", Constants.CONTENT_TYPE_JSON);
 
+        OpenPayURequest orderCreateRequest = prepareOrderCreateRequest();
+
+        HttpResponse response = sender.sendPost(url, JSONConverter.convertToJSON(orderCreateRequest), headers);
+        Map<FieldNames, String> result = httpResponseUtils.parseResponse(response);
+
+        OrderCreateResponse orderCreateResponse = (OrderCreateResponse) JSONConverter.parseResponse(result.get(FieldNames.KEY_PAYLOAD), ResponseType.ORDER_CREATE_RESPONSE);
+
+        LOGGER.log(Level.INFO, "Sending 'POST' request to URL : " + url +
+                               "\nCurrent POS : " + login +
+                               "\nHTTP Status: " + result.get(FieldNames.KEY_STATUS) +
+                               "\nResponse: " + result.get(FieldNames.KEY_PAYLOAD));
+    }
+
+    private OrderCreateRequest prepareOrderCreateRequest() {
         List<Product> productList = new ArrayList<Product>();
         productList.add(new ProductBuilder().withName("product1").withUnitPrice(111).withQuantity(1).build());
 
@@ -55,20 +73,12 @@ public class SenderRealTest extends AbstractTest {
                 .withPhone("123456789")
                 .build();
 
-        OrderCreateRequest orderCreateRequest = new OrderCreateRequestBuilder()
+        return new OrderCreateRequestBuilder()
                 .withTotalAmount(100).withDescription("description")
                 .withAdditionalDescription("additional description")
                 .withProducts(productList).withCurrencyCode("PLN")
                 .withCustomerIP("46.238.126.146").withMerchantPosId("145227")
                 .withBuyer(buyer).withContinueUrl("http://google.pl")
                 .build();
-
-        HttpResponse response = sender.sendPost(url, JSONConverter.convertToJSON(orderCreateRequest), headers);
-        Map<FieldNames, String> result = httpResponseUtils.parseResponse(response);
-
-        LOGGER.log(Level.INFO, "Sending 'POST' request to URL : " + url +
-                               "\nCurrent POS : " + login +
-                               "\nHTTP Status: " + result.get(FieldNames.KEY_STATUS) +
-                               "\nResponse: " + result.get(FieldNames.KEY_PAYLOAD));
     }
 }
---
+++ src/test/java/com/payu/sdk/TestValues.java
@@ -0,0 +1,8 @@
+package com.payu.sdk;
+
+public class TestValues {
+    public static final String jsonResponse =
+            "{\"orderId\":\"LWJ87PXCL5150518GUEST000P01\"," +
+             "\"status\":{\"statusCode\":\"SUCCESS\"}," +
+             "\"redirectUri\":\"https://secure.payu.com/pl/standard/co/summary?sessionId=YLUAdCBxfuTCZLXyVby83fJEN1tYg5l9&merchantPosId=145227&timeStamp=1431968538863&showLoginDialog=false&apiToken=a04eae4ff8bb7ffcea8d2321f908b71cda1fcf0cd7a5f67ad0860c2243b1bf75\"}";
+}
